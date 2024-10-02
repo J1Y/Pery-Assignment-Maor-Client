da6e7661af3a4825daf2d6cfdc0d7609ed974af6
J1Y <maorc56@gmail.com>
2024-10-02 11:57:26 +0300

    trying to make vercel work

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,98 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { FormEvent, useRef, useState } from "react";
+
+const SERVER_URL = "https://pery-assignment-maor-server.vercel.app:3000";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const usernameRef = useRef<HTMLInputElement | null>(null);
+  const signupLanguageRef = useRef<HTMLInputElement | null>(null);
+  const articleNameRef = useRef<HTMLInputElement | null>(null);
+  const articleLanguageRef = useRef<HTMLInputElement | null>(null);
+  const tokenRef = useRef<HTMLInputElement | null>(null);
+
+  const [signupData, setSignupData] = useState("");
+  const [articleData, setArticleData] = useState<any | null>(null);
+  const [signupError, setSignupError] = useState("");
+  const [articleError, setArticleError] = useState("");
+
+  const handleSignup = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const username = usernameRef.current?.value;
+    const language = signupLanguageRef.current?.value;
+    if (!username) {
+      setSignupError("username is required");
+      setSignupData("");
+    }
+
+    const response = await fetch(`${SERVER_URL}/user`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        userName: username,
+        language,
+      }),
+    });
+
+    if (!response.ok) {
+      setSignupError(`something went wrong. error code: ${response.status}`);
+    }
+
+    setSignupError("");
+    setSignupData(await response.text());
+  };
+
+  const handleArticle = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const articleName = articleNameRef.current?.value;
+    const articleLanguage = articleLanguageRef.current?.value;
+    const token = tokenRef.current?.value;
+    if (!articleName) {
+      setArticleError("article name is required");
+      setArticleData("");
+    }
+
+    const response = await fetch(`${SERVER_URL}/introduction/${articleName}`, {
+      method: "GET",
+      headers: {
+        "accept-language": articleLanguage || "",
+        "x-authentication": token || "",
+      },
+    });
+
+    if (!response.ok) {
+      setArticleError(`something went wrong. error: ${await response.text}`);
+    }
+
+    setArticleError("");
+    setArticleData(await response.text());
+  };
+  console.log(import.meta.env.SERVER);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h4>signup:</h4>
+      <form
+        onSubmit={handleSignup}
+        style={{ display: "flex", flexDirection: "row" }}
+      >
+        <input ref={usernameRef} placeholder="username" required />
+        <input ref={signupLanguageRef} placeholder="language. default: en" />
+        <button type="submit">submit</button>
+      </form>
+      {signupError ? signupError : <></>}
+      {signupData ? signupData : <></>}
+      <h4>get article:</h4>
+      <form onSubmit={handleArticle}>
+        <input ref={articleNameRef} placeholder="article name" required />
+        <input ref={articleLanguageRef} placeholder="language. default: en" />
+        <input ref={tokenRef} placeholder="token. will override language" />
+        <button type="submit">submit</button>
+      </form>
+      {articleError ? articleError : <></>}
+      {articleData ? articleData : <></>}
+    </div>
+  );
 }
 
-export default App
+export default App;
